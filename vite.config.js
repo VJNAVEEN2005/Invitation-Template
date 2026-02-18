7407510f40cfd800d68ffa78c933fd932cd0fa83
Naveen <vjnaveen.fe4@gmail.com>
2026-02-18 14:23:54 +0530

    feat: Configure SPA redirects and dynamic base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   base: '/Invitation-Template/',  
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/Invitation-Template/' : '/',
+  }
 })
